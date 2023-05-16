d428fc12d6e051036d62aee60713af2125817cee
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2023-05-16 10:32:39 -0600

    add: added components, composables and set tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
-    <h1 class="text-red-300">My App</h1>
+  <div :class="{ dark: darkMode }">
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div
+          class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
+        >
+          <!-- Left Sidebar -->
+
+          <div class="hidden md:block xs-col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <Left />
+            </div>
+          </div>
+
+          <!-- Main Content -->
+
+          <!-- Right SideBar -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<script setup>
+const darkMode = ref(false);
+</script>
---
+++ components/Left.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="h-screen flex-col flex">
+    <div
+      class="p-2 my-2 rounded-full hover:bg-blue-50 w-min dark:hover:bg-white/20"
+      :class="defaultTransition"
+    >
+      <NuxtLink to="/">
+        <div class="w-8 h-8">
+          <LogoTwitter />
+        </div>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+const { defaultTransition } = useTailwindConfig();
+</script>
